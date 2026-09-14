e30f820f8dda59e9c8f4366acc3874ca8827e292
Ehteshamul Hoq Mahi <ehteshamulhoqmahi@gmail.com>
2026-09-14 19:27:58 +0600

    1st day

---
+++ even_odd_check.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter your number: ";
+    cin >> x;
+
+    if (x % 2 == 0)
+        cout << "The number is even." << endl;
+    else
+        cout << "The number is odd." << endl;
+
+    return 0;
+}
